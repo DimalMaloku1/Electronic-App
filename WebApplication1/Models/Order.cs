6e9501eb2ab0e9c5d579f868b40a0068a9250c30
DimalMaloku1 <dm51522@ubt-uni.net>
2023-04-15 17:16:19 +0200

    test

---
+++ WebApplication1/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        [ForeignKey(nameof(UserId))]
+
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ WebApplication1/Models/ShoppingCartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+        public string ShoppingCartId { get; set; }
+    }
+}
