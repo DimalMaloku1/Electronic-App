d01b3f3576e068fe737eba3f8a1496446250b2a5
AndiZogaj22 <az51520@ubt-uni.net>
2023-04-26 15:55:24 +0200

    class fixed

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -5,22 +5,25 @@ import {Cart} from './pages/Cart'
 import {Wishlist} from './pages/Wishlist'
 import {Signup} from './pages/Signup'
 import {Contact} from './pages/Contact'
-
-
+import {Dashboard} from '../src/pages/admin/Dashboard'
+import {Statistics} from '../src/pages/admin/Statistics'
+import {Users} from '../src/pages/admin/Users'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        
+      <Navbar />         
+
         <Routes>
           <Route path="/" element={<Shop />}/>
           <Route path="cart"  element={<Cart />}/>
           <Route path="wishlist" element={<Wishlist />} />
           <Route path="signup" element={<Signup />} />
           <Route path="contact" element={<Contact />} />
-          
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="statistics" element={<Statistics />}/>
+          <Route path="users"  element={<Users />}/>
         </Routes>
       </Router>
     </div>
