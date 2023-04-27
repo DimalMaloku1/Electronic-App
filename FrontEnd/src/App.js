f99ded5291e60a717df3a7bc084fe7601e5e8833
AndiZogaj22 <az51520@ubt-uni.net>
2023-04-27 20:00:39 +0200

    new routes added

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -8,6 +8,10 @@ import {Contact} from './pages/Contact'
 import {Dashboard} from '../src/pages/admin/Dashboard'
 import {Statistics} from '../src/pages/admin/Statistics'
 import {Users} from '../src/pages/admin/Users'
+import {Adminproducts} from "./pages/admin/Adminproducts";
+import {Categories} from "./pages/admin/Categories";
+import {Orders} from "./pages/admin/Orders";
+import {Messages} from "./pages/admin/Messages";
 
 function App() {
   return (
@@ -24,6 +28,11 @@ function App() {
           <Route path="Dashboard" element={<Dashboard />} />
           <Route path="statistics" element={<Statistics />}/>
           <Route path="users"  element={<Users />}/>
+          <Route path="adminproducts"  element={<Adminproducts />}/>
+          <Route path="categories"  element={<Categories />}/>
+          <Route path="orders"  element={<Orders />}/>
+          <Route path="messages"  element={<Messages />}/>
+
         </Routes>
       </Router>
     </div>
