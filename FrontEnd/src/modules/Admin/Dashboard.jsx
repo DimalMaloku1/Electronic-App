817a7bb9fdc7cf5eae7cabba5d4fb65735a81198
DimalMaloku1 <dm51522@ubt-uni.net>
2023-05-14 21:59:16 +0200

    Admin Error Fixed

---
+++ FrontEnd/src/modules/Admin/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import  Sidebar  from './Sidebar'
+import './SidebarData'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <>      
+      <Sidebar />
+      <h1>Admin dashboard test</h1>
+      </>      
+    </div>
+  )
+}
+
+export default Dashboard
