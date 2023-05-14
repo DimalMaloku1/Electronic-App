72afab0b4e3848ddeed032b9d1d081eaeb0c1106
DESKTOP-509JDPP\38343 <dh53689@ubt-uni.net>
2023-05-14 15:27:35 +0200

    Page Contact is added

--- FrontEnd/src/modules/Contact/Contact.jsx
+++ FrontEnd/src/modules/Contact/Contact.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div>Contact test</div>
-  )
-}
+    <div className="max-w-md mx-auto py-12">
+      <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
+      <form>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+            Name
+          </label>
+          <input
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="message"
+            rows="5"
+            placeholder="Enter your message"
+          ></textarea>
+        </div>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
