5bcf9573b62e1a069a0d392d80e31d3bd5ca8dc5
DESKTOP-509JDPP\38343 <dh53689@ubt-uni.net>
2023-05-14 15:32:41 +0200

    Admin pages is added to the project

---
+++ FrontEnd/src/modules/admin/Categories.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Sidebar from './Sidebar';
+import './Dashboard.css';
+
+
+export function Categories() {
+  return (
+    <div className='categories-container'>
+      <>
+      <Sidebar />
+      <h1>Categories page</h1>
+    </>
+    </div>
+  );
+}
+
+export default Categories;
---
+++ FrontEnd/src/modules/admin/SidebarData.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Statistics',
+    path: '/statistics',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Users',
+    path: '/users',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'AdminProducts',
+    path: '/adminproducts',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Categories',
+    path: '/categories',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Orders',
+    path: '/orders',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Messages',
+    path: '/messages',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+];
