c61a4d7e5032ba0a3060f054218ce696869547fa
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-21 19:46:31 +0200

    product filter by categories fixed

--- FrontEnd/src/modules/CategoryProducts/index.js
+++ FrontEnd/src/modules/CategoryProducts/index.js
@@ -7,7 +7,7 @@ const CategoryProducts = () => {
   const [products, setProducts] = useState([])
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch(`https://fakestoreapi.com/products/category/${name}`)
+      const response = await fetch(`https://localhost:7099/api/Products/category/${name}`)
       const data = await response.json()
       console.log(data)
       setProducts(data)
