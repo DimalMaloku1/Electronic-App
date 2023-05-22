e0cf71c2ebb649a049f870ae1bdb173b1a9ffdef
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-22 21:47:50 +0200

    admin page content added

---
+++ FrontEnd/src/adminComponents/PageContent/index.js
@@ -0,0 +1,12 @@
+/*
+import AppRoutes from "../AppRoutes";
+
+function PageContent() {
+  return (
+    <div className="PageContent">
+      <AppRoutes />
+    </div>
+  );
+}
+export default PageContent;
+*/
