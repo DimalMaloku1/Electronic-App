a8aed5be911b72c35b4fb6b9068b091978fefd2f
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-22 21:48:08 +0200

    admin side menu added

---
+++ FrontEnd/src/adminComponents/SideMenu/index.js
@@ -0,0 +1,78 @@
+/*
+import {
+    AppstoreOutlined,
+    ShopOutlined,
+    ShoppingCartOutlined,
+    UserOutlined,
+    MessageOutlined,
+    LogoutOutlined,
+    DropboxOutlined
+  } from "@ant-design/icons";
+  import { Menu } from "antd";
+  import { useEffect, useState } from "react";
+  import { useLocation, useNavigate } from "react-router-dom";
+  
+  function SideMenu() {
+    const location = useLocation();
+    const [selectedKeys, setSelectedKeys] = useState("/");
+  
+    useEffect(() => {
+      const pathName = location.pathname;
+      setSelectedKeys(pathName);
+    }, [location.pathname]);
+  
+    const navigate = useNavigate();
+    return (
+      <div className="SideMenu">
+        <Menu
+          className="SideMenuVertical"
+          mode="vertical"
+          onClick={(item) => {
+            //item.key
+            navigate(item.key);
+          }}
+          selectedKeys={[selectedKeys]}
+          items={[
+            {
+              label: "Dashbaord",
+              icon: <AppstoreOutlined />,
+              key: "/",
+            },
+            {
+              label: "Admin Products",
+              key: "/adminproducts",
+              icon: <ShopOutlined />,
+            },
+            {
+              label: "Orders",
+              key: "/orders",
+              icon: <ShoppingCartOutlined />,
+            },
+            {
+              label: "Customers",
+              key: "/customers",
+              icon: <UserOutlined />,
+            },
+          
+            {
+              label: "Messages",
+              key: "/messages",
+              icon: <MessageOutlined />,
+            },
+            {
+              label: "Categories",
+              key: "/categories",
+              icon:<DropboxOutlined />,
+            },
+            {
+              label: "Log Out",
+              key: "/logout",
+              icon: <LogoutOutlined />,
+            },
+          ]}
+        ></Menu>
+      </div>
+    );
+  }
+  export default SideMenu;
+*/  
