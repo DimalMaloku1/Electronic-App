6f89e46520b8ead19e9ba7dec06540e4f6564b2e
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-22 21:47:31 +0200

    new admin routes added

---
+++ FrontEnd/src/adminComponents/AdminRoutes/index.js
@@ -0,0 +1,22 @@
+/*import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Customers from "../../Pages/Customers";
+import Dashboard from "../../Pages/Dashbaord";
+import AdminProducts from "../../Pages/AdminProducts";
+import Orders from "../../Pages/Orders";
+import Messages from "../../Pages/Messages/Messages";
+import Categories from "../../Pages/Categories/Categories";
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/adminproducts" element={<AdminProducts />}></Route>
+      <Route path="/orders" element={<Orders />}></Route>
+      <Route path="/customers" element={<Customers />}></Route>
+      <Route path="/messages" element={<Messages />}></Route>
+      <Route path="/categories" element={<Categories />}></Route>
+
+    </Routes>
+  );
+}
+export default AppRoutes;
+*/
