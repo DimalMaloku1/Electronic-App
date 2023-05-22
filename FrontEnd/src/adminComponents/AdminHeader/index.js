7e7b4ee21dc87249d2967b6415c69890d6b31b4c
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-22 21:47:02 +0200

    admin header added

---
+++ FrontEnd/src/adminComponents/AdminHeader/index.js
@@ -0,0 +1,19 @@
+/*
+import { Badge, Drawer, Image, List, Space, Typography } from "antd";
+import PfpImg from '../../Assets/AdminPfp.png'
+
+function AppHeader() {
+  
+  return (
+    <div className="AppHeader">
+      <Image
+        width={40}
+        src={PfpImg}></Image>
+      <Typography.Title ><div className="admin-title">Admin's Dashboard</div></Typography.Title>
+      <Space>
+      </Space>
+    </div>
+  );
+}
+export default AppHeader;
+*/
