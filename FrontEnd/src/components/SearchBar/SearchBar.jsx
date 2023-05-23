c4cc8f83222c9f6f8d6cfa2f671baeda1da2663c
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-23 02:19:21 +0200

    search bar done

--- FrontEnd/src/components/SearchBar/SearchBar.jsx
+++ FrontEnd/src/components/SearchBar/SearchBar.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBar = ({ handleSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   const handleChange = (event) => {
-    const searchTerm = event.target.value;
-    handleSearch(searchTerm);
+    setSearchTerm(event.target.value);
+    handleSearch(event.target.value);
   };
 
   return (
-    <div className="flex justify-center items-center mb-4">
-      <div className="flex items-center">
-        <input
-          type="text"
-          placeholder="Search products..."
-          onChange={handleChange}
-          className="bg-white border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />
-        <button
-          className="ml-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
-          onClick={handleSearch}
-        >
-          Search
-        </button>
-      </div>
+    <div className="flex items-center justify-center mb-6">
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={searchTerm}
+        onChange={handleChange}
+        className="border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-400"
+      />
+      <button
+        onClick={() => {
+          setSearchTerm('');
+          handleSearch('');
+        }}
+        className="ml-2 px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+      >
+        Clear
+      </button>
     </div>
   );
 };
--- FrontEnd/src/modules/Products/index.js
+++ FrontEnd/src/modules/Products/index.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Categories from '../../components/Categories'
 import ProductCard from '../../components/ProductCard'
-import SearchBar from '../../components/SearchBar/SearchBar'
 
 const Products = () => {
   const [products, setProducts] = useState([])
-  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -13,37 +11,20 @@ const Products = () => {
       const data = await response.json()
       console.log(data)
       setProducts(data)
-      setFilteredProducts(data); // Set filtered products initially to all products
 
     }
     fetchProducts()
   }, [])
 
 
-  const handleSearch = (searchTerm) => {
-    // Filter products based on the entered search term
-    const filtered = products.filter((product) =>
-      product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredProducts(filtered);
-  };
+ 
   return (
     <div>
       <Categories/>
       <div className="flex flex-col text-center w-full mt-20">
         <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">PRODUCTS</h2>
         <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">ALL PRODUCTS</h1>
-        <SearchBar handleSearch={handleSearch} />
-        <div className="grid grid-cols-3 gap-4">
-        {/* Render product cards */}
-        {filteredProducts.map((product) => (
-          <div key={product.id} className="bg-white p-4 rounded-md shadow">
-            <h3 className="text-lg font-bold mb-2">{product.name}</h3>
-            <p>{product.description}</p>
-            <p className="mt-2">${product.price}</p>
-          </div>
-        ))}
-     </div>
+       
      </div>
       {
         products.length > 0 ?
