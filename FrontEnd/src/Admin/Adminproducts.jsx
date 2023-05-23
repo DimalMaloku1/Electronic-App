31d2cc25d7bd65b062c6f46f2135e62dc609ab7e
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-23 20:32:29 +0200

    admin pages added

---
+++ FrontEnd/src/Admin/Adminproducts.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import PageContent from "../adminComponents/PageContent";
+import SideMenu from "../adminComponents/SideMenu";
+
+const Adminproducts = () => {
+  return (
+    <div>
+      
+      <div className="SideMenuAndPageContent">
+        <SideMenu></SideMenu>
+        <PageContent></PageContent>
+      </div>
+    </div>
+  )
+}
+
+export default Adminproducts
