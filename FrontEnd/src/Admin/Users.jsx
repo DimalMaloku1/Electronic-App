3b5786dd078bf61f8bc56e524e40163c57975c20
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-24 21:36:56 +0200

    admin users table

--- FrontEnd/src/Admin/Users.jsx
+++ FrontEnd/src/Admin/Users.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import PageContent from "../adminComponents/PageContent";
 import SideMenu from "../adminComponents/SideMenu";
-
+import UsersTable from '../adminComponents/UsersTable';
 const Users = () => {
   return (
     <div>
       <div className="SideMenuAndPageContent">
         <SideMenu></SideMenu>
         <PageContent></PageContent>
+        <UsersTable />
       </div>
     </div>
   )
