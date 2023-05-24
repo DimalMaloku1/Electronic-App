1da047b4ed4c85bb3febbd2e80d85c9d30bd97a3
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-24 12:39:09 +0200

    listing page for CRUD in admin page

---
+++ FrontEnd/src/Admin/ProductsListing.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const ProductsListing = () => {
+    const [productsdata, productsdatachange] = useState(null);
+    const navigate = useNavigate();
+
+    
+    const LoadEdit = (id) => {
+        navigate("/products/edit/" + id);
+    }
+    const Removefunction = (id) => {
+        if (window.confirm('Do you want to remove?')) {
+            fetch("https://localhost:7099/api/Products/" + id, {
+                method: "DELETE"
+            }).then((res) => {
+                alert('Removed successfully.')
+                window.location.reload();
+            }).catch((err) => {
+                console.log(err.message)
+            })
+        }
+    }
+
+
+
+
+    useEffect(() => {
+        fetch("https://localhost:7099/api/Products").then((res) => {
+            return res.json();
+        }).then((resp) => {
+            productsdatachange(resp);
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }, [])
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-title">
+                    <h2>Products Listing</h2>
+                </div>
+                <div className="card-body">
+                    <div className="divbtn">
+                        <Link to="products/create" className="btn btn-success">Add New (+)</Link>
+                    </div>
+                    <table className="table table-bordered">
+                        <thead className="bg-dark text-white">
+                            <tr>
+                                <td>ID</td>
+                                <td>Name</td>
+                                <td>Description</td>
+                                <td>Price</td>
+                                <td>Stock</td>
+                                <td>Image</td>
+                                <td>Category</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {productsdata &&
+                                productsdata.map(item => (
+                                    <tr key={item.id}>
+                                        <td>{item.id}</td>
+                                        <td>{item.name}</td>
+                                        <td>{item.description}</td>
+                                        <td>{item.price}</td>
+                                        <td>{item.stock}</td>
+                                        <td>{item.imageURL}</td>
+                                        <td>{item.categoryName}</td>
+                                        <td><a onClick={() => { LoadEdit(item.id) }} className="btn btn-success">Edit</a>
+                                            <a onClick={() => { Removefunction(item.id) }} className="btn btn-danger">Remove</a>
+                                            
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductsListing;
