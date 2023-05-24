0d0875066d56b471389fa595ec6cd247f86031f3
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-24 21:36:11 +0200

    messages page work

--- FrontEnd/src/Admin/Messages.jsx
+++ FrontEnd/src/Admin/Messages.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import PageContent from "../adminComponents/PageContent";
 import SideMenu from "../adminComponents/SideMenu";
-
+import MessageTable from '../adminComponents/MessagesTable';
 const Messages = () => {
   return (
     <div>
       <div className="SideMenuAndPageContent">
         <SideMenu></SideMenu>
         <PageContent></PageContent>
+        <MessageTable />
       </div>
     </div>
   )
