0a0fa2cdb27e82368e8176a09e53381a7a2ca65d
DimalMaloku1 <dm51522@ubt-uni.net>
2023-05-25 22:34:58 +0200

    Added Role Property in UserDTO

--- BackEnd/ElectroShop/DTO/UserDTO.cs
+++ BackEnd/ElectroShop/DTO/UserDTO.cs
@@ -4,6 +4,6 @@
     {
         public string Id { get; set; }
         public string Email { get; set; }
-        public List<string> Roles { get; internal set; }
+        public string Role { get; internal set; }
     }
 }
