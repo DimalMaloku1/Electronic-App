b4f2f27e949881c03ee12080141ef1f9e344764e
DESKTOP-509JDPP\38343 <dh53689@ubt-uni.net>
2023-05-26 20:11:13 +0200

    Orders table added

--- FrontEnd/src/Admin/Orders.jsx
+++ FrontEnd/src/Admin/Orders.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PageContent from "../adminComponents/PageContent";
 import SideMenu from "../adminComponents/SideMenu";
+import OrderTable from '../adminComponents/OrdersTable';
 
 const Orders = () => {
   return (
@@ -8,6 +9,7 @@ const Orders = () => {
       <div className="SideMenuAndPageContent">
         <SideMenu></SideMenu>
         <PageContent></PageContent>
+        <OrderTable />
       </div>
     </div>
   )
