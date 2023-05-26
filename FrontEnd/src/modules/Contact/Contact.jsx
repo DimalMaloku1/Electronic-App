1978a102a499252c371377fe190784c439308eeb
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-26 19:53:55 +0200

    working on the contact form validation

--- FrontEnd/src/modules/Contact/Contact.jsx
+++ FrontEnd/src/modules/Contact/Contact.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
 
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+//https://localhost:7099/api/Contacts api per me kriju mesazh
 const Contact = () => {
+
+
+  const [formData, setFormData] = useState({ name: '', email: '', phone: '', message: '' });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform final validation checks on formData if needed
+
+    // Make an API request to send the message data to the backend
+    axios.post('https://localhost:7099/api/Contacts', formData)
+      .then((response) => {
+        // Handle the response if needed (e.g., display a success message)
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Handle any errors that occur during the request
+        console.error(error);
+      });
+  };
   return (
     <div className="max-w-md mx-auto py-12">
       <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
-      <form>
+      <form  onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="name" >
             Name
           </label>
           <input
@@ -14,6 +43,7 @@ const Contact = () => {
             id="name"
             type="text"
             placeholder="Enter your name"
+            value={formData.name} onChange={handleInputChange}
           />
         </div>
         <div className="mb-4">
@@ -25,6 +55,7 @@ const Contact = () => {
             id="email"
             type="email"
             placeholder="Enter your email"
+            value={formData.email} onChange={handleInputChange}
           />
         </div>
         <div className="mb-4">
@@ -36,6 +67,7 @@ const Contact = () => {
             id="number"
             type="number"
             placeholder="Enter your number"
+            value={formData.phone} onChange={handleInputChange}
           />
         </div>
         <div className="mb-4">
@@ -47,6 +79,7 @@ const Contact = () => {
             id="message"
             rows="5"
             placeholder="Enter your message"
+            value={formData.message} onChange={handleInputChange}
           ></textarea>
         </div>
         <button
