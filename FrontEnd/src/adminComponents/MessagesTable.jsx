89a31b88227bbb3f9387afc480d4a5a6743edb4f
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-26 19:26:11 +0200

    messages inputs and table

--- FrontEnd/src/adminComponents/MessagesTable.jsx
+++ FrontEnd/src/adminComponents/MessagesTable.jsx
@@ -21,19 +21,19 @@ const MessageTable = () => {
     <table className="min-w-full border-collapse">
       <thead>
         <tr>
-          <th className="py-2 px-1 border-b">ID</th>
-          <th className="py-2 px-1 border-b">Subject</th>
-          <th className="py-2 px-1 border-b">Sender</th>
-          <th className="py-2 px-1 border-b">Date</th>
+          <th className="py-2 px-1 border-b">Name</th>
+          <th className="py-2 px-1 border-b">Email</th>
+          <th className="py-2 px-1 border-b">Phone</th>
+          <th className="py-2 px-1 border-b">Message</th>
         </tr>
       </thead>
       <tbody>
         {messages.map(message => (
           <tr key={message.id}>
-            <td className="py-2 px-1 border-b">{message.id}</td>
-            <td className="py-2 px-1 border-b">{message.subject}</td>
-            <td className="py-2 px-1 border-b">{message.sender}</td>
-            <td className="py-2 px-1 border-b">{message.date}</td>
+            <td className="py-2 px-1 border-b">{message.name}</td>
+            <td className="py-2 px-1 border-b">{message.email}</td>
+            <td className="py-2 px-1 border-b">{message.phone}</td>
+            <td className="py-2 px-1 border-b">{message.message}</td>
           </tr>
         ))}
       </tbody>
