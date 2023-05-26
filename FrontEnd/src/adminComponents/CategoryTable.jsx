25e0b790f01ecfb25b7480d134aff2b0aa17ffe0
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-26 21:04:34 +0200

    category table functions added and styling fixed

--- FrontEnd/src/adminComponents/CategoryTable.jsx
+++ FrontEnd/src/adminComponents/CategoryTable.jsx
@@ -1,44 +1,79 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const CategoryTable = () => {
-  const [category, setcategory] = useState([]);
+    const [categoriesdata, categoriesdatachange] = useState(null);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    fetchcategory();
-  }, []);
-
-  const fetchcategory = async () => {
-    try {
-      const response = await fetch('https://fakestoreapi.com/products'); // Replace with your API endpoint
-      const data = await response.json();
-      setcategory(data);
-    } catch (error) {
-      console.log('Error fetching users:', error);
+    
+    const LoadEdit = (id) => {
+        navigate("/categories/edit/" + id);
     }
-  };
+    const Removefunction = (id) => {
+        if (window.confirm('Do you want to remove?')) {
+            fetch("https://localhost:7099/api/Categories/" + id, {
+                method: "DELETE"
+            }).then((res) => {
+                alert('Removed successfully.')
+                window.location.reload();
+            }).catch((err) => {
+                console.log(err.message)
+            })
+        }
+    }
 
-  return (
-    <div className="max-w-1x1 mx-1 p-1">
-      <table className="w-full bg-white">
-        <thead>
-          <tr>
-            <th className="py-2 px-4 border-b">ID</th>
-            <th className="py-2 px-4 border-b">Category Name</th>
-           
-          </tr>
-        </thead>
-        <tbody>
-          {category.map(category => (
-            <tr key={category.id}>
-              <td className="py-2 px-4 border-b">{category.id}</td>
-              <td className="py-2 px-4 border-b">{category.title}</td>
-           
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+
+
+
+    useEffect(() => {
+        fetch("https://localhost:7099/api/Categories").then((res) => {
+            return res.json();
+        }).then((resp) => {
+          categoriesdatachange(resp);
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }, [])
+
+    const buttonStylesAdd = `px-4 py-2 rounded-md text-white bg-emerald-800`;
+    const buttonStylesEdit = `px-4 py-2 rounded-md text-white bg-yellow-400	`;
+    const buttonStylesRemove = `px-4 py-2 rounded-md text-white bg-rose-600	`;
+
+
+    return (
+        <>
+         <button className={buttonStylesAdd} color="blue">
+                        <Link to="/categories/create">Add New </Link>
+                    </button>
+        <table className="min-w-full border-collapse">
+      
+      <thead>
+        <tr>
+          <th className="py-2 px-4 border-b w-1/6">ID</th>
+          <th className="py-2 px-4 border-b w-1/6">Category Name</th>
+          
+
+        </tr>
+      </thead>
+      <tbody>
+      {categoriesdata &&
+                                categoriesdata.map(categories => (
+                                    <tr key={categories.id}>
+                                        <td className="w-1/6">{categories.id}</td>
+                                        <td className="w-1/6">{categories.name}</td>
+                                       
+                                        <td className="w-1/6"><button onClick={() => { LoadEdit(categories.id) }} className={buttonStylesEdit} color="yellow">Edit</button>
+                                                               <button onClick={() => { Removefunction(categories.id) }} className={buttonStylesRemove} color="red">Remove</button>
+                                            
+                                            
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+      </tbody>
+    </table>
+    </>
+    );
+}
 
 export default CategoryTable;
