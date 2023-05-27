12cc96f1d982ec9df4c75e455dc57e539d7a3c14
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-27 13:09:10 +0200

    login validation added

--- FrontEnd/src/modules/LoginRegister/Login.jsx
+++ FrontEnd/src/modules/LoginRegister/Login.jsx
@@ -1,19 +1,98 @@
-import React from "react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, usernameupdate] = useState('');
+  const [password, passwordupdate] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Your login logic goes here
-  };
+  const usenavigate=useNavigate();
 
+  useEffect(()=>{
+sessionStorage.clear();
+  },[]);
+
+  const ProceedLogin = (e) => {
+      e.preventDefault();
+      if (validate()) {
+          ///implentation
+          // console.log('proceed');
+          fetch("http://localhost:8000/user/" + username).then((res) => {
+              return res.json();
+          }).then((resp) => {
+              //console.log(resp)
+              if (Object.keys(resp).length === 0) {
+                  toast.error('Please Enter valid username');
+              } else {
+                  if (resp.password === password) {
+                      toast.success('Success');
+                      sessionStorage.setItem('username',username);
+                      sessionStorage.setItem('userrole',resp.role);
+                      usenavigate('/')
+                  }else{
+                      toast.error('Please Enter valid credentials');
+                  }
+              }
+          }).catch((err) => {
+              toast.error('Login Failed due to :' + err.message);
+          });
+      }
+  }
+
+  const ProceedLoginusingAPI = (e) => {
+      e.preventDefault();
+      if (validate()) {
+          ///implentation
+          // console.log('proceed');
+          let inputobj={"username": username,
+          "password": password};
+          fetch("https://localhost:44308/User/Authenticate",{
+              method:'POST',
+              headers:{'content-type':'application/json'},
+              body:JSON.stringify(inputobj)
+          }).then((res) => {
+              return res.json();
+          }).then((resp) => {
+              console.log(resp)
+              if (Object.keys(resp).length === 0) {
+                  toast.error('Login failed, invalid credentials');
+              }else{
+                   toast.success('Success');
+                   sessionStorage.setItem('username',username);
+                   sessionStorage.setItem('jwttoken',resp.jwtToken);
+                 usenavigate('/')
+              }
+              // if (Object.keys(resp).length === 0) {
+              //     toast.error('Please Enter valid username');
+              // } else {
+              //     if (resp.password === password) {
+              //         toast.success('Success');
+              //         sessionStorage.setItem('username',username);
+              //         usenavigate('/')
+              //     }else{
+              //         toast.error('Please Enter valid credentials');
+              //     }
+              // }
+          }).catch((err) => {
+              toast.error('Login Failed due to :' + err.message);
+          });
+      }
+  }
+  const validate = () => {
+      let result = true;
+      if (username === '' || username === null) {
+          result = false;
+          toast.warning('Please Enter Username');
+      }
+      if (password === '' || password === null) {
+          result = false;
+          toast.warning('Please Enter Password');
+      }
+      return result;
+  }
   return (
     <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="bg-white p-10 rounded-lg shadow-lg">
+      <form onSubmit={ProceedLogin} className="bg-white p-10 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold mb-5">Login</h2>
         <div className="mb-5">
           <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
@@ -23,9 +102,7 @@ const Login = () => {
             type="email"
             id="email"
             className="border-2 border-gray-400 p-2 w-full rounded-md"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
+            value={username} onChange={e => usernameupdate(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -33,12 +110,9 @@ const Login = () => {
             Password
           </label>
           <input
-            type="password"
             id="password"
             className="border-2 border-gray-400 p-2 w-full rounded-md"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
+            type="password" value={password} onChange={e => passwordupdate(e.target.value)}
           />
         </div>
         <div className="flex justify-between items-center">
