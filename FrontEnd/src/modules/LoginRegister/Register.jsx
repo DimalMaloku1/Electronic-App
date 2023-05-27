4992abbe70dac9b58e65f0ad6ff5bccb061dfdf8
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-27 13:18:14 +0200

    working on register validation

--- FrontEnd/src/modules/LoginRegister/Register.jsx
+++ FrontEnd/src/modules/LoginRegister/Register.jsx
@@ -1,32 +1,70 @@
-import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Register = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [password, passwordchange] = useState("");
+  const [email, emailchange] = useState("");
+ 
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Your register logic goes here
-  };
+  const navigate = useNavigate();
 
+  const IsValidate = () => {
+      let isproceed = true;
+      let errormessage = 'Please enter the value in ';
+     
+      if (password === null || password === '') {
+          isproceed = false;
+          errormessage += ' Password';
+      }
+      if (email === null || email === '') {
+          isproceed = false;
+          errormessage += ' Email';
+      }
+
+      if(!isproceed){
+          toast.warning(errormessage)
+      }else{
+          if(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)){
+
+          }else{
+              isproceed = false;
+              toast.warning('Please enter the valid email')
+          }
+      }
+      return isproceed;
+  }
+
+
+  const handlesubmit = (e) => {
+          e.preventDefault();
+          let regobj = {  password, email,};
+          if (IsValidate()) {
+          //console.log(regobj);
+          fetch("http://localhost:8000/user", {
+              method: "POST",
+              headers: { 'content-type': 'application/json' },
+              body: JSON.stringify(regobj)
+          }).then((res) => {
+              toast.success('Registered successfully.')
+              navigate('/login');
+          }).catch((err) => {
+              toast.error('Failed :' + err.message);
+          });
+      }
+  }
   return (
     <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="bg-white p-10 rounded-lg shadow-lg">
+      <form onSubmit={handlesubmit} className="bg-white p-10 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold mb-5">Create an account</h2>
         <div className="mb-5">
           <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
             Email
           </label>
           <input
-            type="email"
             id="email"
             className="border-2 border-gray-400 p-2 w-full rounded-md"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
+            value={email} onChange={e => emailchange(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -34,27 +72,12 @@ const Register = () => {
             Password
           </label>
           <input
-            type="password"
             id="password"
             className="border-2 border-gray-400 p-2 w-full rounded-md"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
+            value={password} onChange={e => passwordchange(e.target.value)} type="password" 
           />
         </div>
-        <div className="mb-5">
-          <label htmlFor="confirm-password" className="block text-gray-700 font-bold mb-2">
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            id="confirm-password"
-            className="border-2 border-gray-400 p-2 w-full rounded-md"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </div>
+        
         <div className="flex justify-between items-center">
           <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md">
             Create account
