280f745969b9d7e45d4af6679f71ce3886a5fd61
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-30 16:28:19 +0200

    admin header added

--- FrontEnd/src/Admin/Adminproducts.jsx
+++ FrontEnd/src/Admin/Adminproducts.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 import PageContent from "../adminComponents/PageContent";
 import SideMenu from "../adminComponents/SideMenu";
 import ProductsListing from './ProductsListing';
+import AppHeader from '../adminComponents/AdminHeader'
+
 
 import './Products.css'
 
 const Adminproducts = () => {
   return (
     <div>
-      
+      <AppHeader />
       <div className="SideMenuAndPageContent">
         <SideMenu></SideMenu>
         <PageContent></PageContent>
