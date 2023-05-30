514ce658e6e4e7dd76a703fcdf313f44ec287110
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-30 16:32:46 +0200

    adming header

--- FrontEnd/src/Admin/Messages.jsx
+++ FrontEnd/src/Admin/Messages.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import PageContent from "../adminComponents/PageContent";
 import SideMenu from "../adminComponents/SideMenu";
 import MessageTable from '../adminComponents/MessagesTable';
+import AppHeader from '../adminComponents/AdminHeader'
+
 const Messages = () => {
   return (
     <div>
+      <AppHeader />
       <div className="SideMenuAndPageContent">
         <SideMenu></SideMenu>
         <PageContent></PageContent>
