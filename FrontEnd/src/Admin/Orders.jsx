1226062797cd4120ebd74f4e4abbe93b6d3082aa
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-30 16:33:04 +0200

    orders

--- FrontEnd/src/Admin/Orders.jsx
+++ FrontEnd/src/Admin/Orders.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import PageContent from "../adminComponents/PageContent";
 import SideMenu from "../adminComponents/SideMenu";
 import OrderTable from '../adminComponents/OrdersTable';
+import AppHeader from '../adminComponents/AdminHeader'
+
 
 const Orders = () => {
   return (
     <div>
+      <AppHeader/>
       <div className="SideMenuAndPageContent">
         <SideMenu></SideMenu>
         <PageContent></PageContent>
