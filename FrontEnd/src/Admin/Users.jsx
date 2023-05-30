f4629ee611872d5a8809c91b2da3c1aad340d8fe
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-30 16:34:20 +0200

    users

--- FrontEnd/src/Admin/Users.jsx
+++ FrontEnd/src/Admin/Users.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import PageContent from "../adminComponents/PageContent";
 import SideMenu from "../adminComponents/SideMenu";
 import UsersTable from '../adminComponents/UsersTable';
+import AppHeader from '../adminComponents/AdminHeader'
+
 const Users = () => {
   return (
     <div>
+      <AppHeader/>
       <div className="SideMenuAndPageContent">
         <SideMenu></SideMenu>
         <PageContent></PageContent>
