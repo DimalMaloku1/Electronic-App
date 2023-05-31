a58a960fb202aef78afe1b42a3c210eb238b33e0
AndiZogaj22 <az51520@ubt-uni.net>
2023-06-01 01:06:21 +0200

    added some styling

--- FrontEnd/src/components/Header/index.js
+++ FrontEnd/src/components/Header/index.js
@@ -32,36 +32,66 @@ const Header = () => {
   return (
     <header className="text-gray-600 body-font shadow-lg">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link to={'/'} className="flex cursor-pointer title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+        <Link to={'/'} className="flex items-center text-gray-900 mb-4 md:mb-0">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">Eelectronic Ecommerce</span>
+          <span className="ml-3 text-xl">Electronic Ecommerce</span>
         </Link>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          {
-            navigations.map((navigation) => {
-              return (
-                <Link to={navigation.path} className="mr-5 hover:text-gray-900">{navigation.name}</Link>
-              )
-            })
-          }
+          {navigations.map((navigation) => (
+            <Link
+              key={navigation.path}
+              to={navigation.path}
+              className="mr-5 hover:text-gray-900"
+            >
+              {navigation.name}
+            </Link>
+          ))}
         </nav>
-        
-        <Link to={'/cart'} className="inline-flex items-center text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-700 rounded text-base mt-4 md:mt-0">Cart
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <Link
+          to={'/cart'}
+          className="inline-flex items-center text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-700 rounded text-base mt-4 md:mt-0"
+        >
+          Cart
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
         </Link>
-        <Link to={'/wishlist'} className="inline-flex items-center text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-700 rounded text-base mt-4 md:mt-0 ml-4 ">WishList
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <Link
+          to={'/wishlist'}
+          className="inline-flex items-center text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-700 rounded text-base mt-4 md:mt-0 ml-4"
+        >
+          Wishlist
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
         </Link>
-        <Link to={'/login'} className="inline-flex items-center text-white bg-sky-600	 border-0 py-2 px-4 focus:outline-none hover:bg-teal-300	 rounded text-base mt-4 md:mt-0 ml-10 ">Login
+        <Link
+          to={'/login'}
+          className="inline-flex items-center text-white bg-sky-600 border-0 py-2 px-4 focus:outline-none hover:bg-teal-300 rounded text-base mt-4 md:mt-0 ml-10"
+        >
+          Login
         </Link>
-       <button className='inline-flex items-center text-white bg-red-600	 border-0 py-2 px-4 focus:outline-none hover:bg-red-300	 rounded text-base mt-4 md:mt-0 ml-10'>LogOut</button>
-      
+        <button className='inline-flex items-center text-white bg-red-600 border-0 py-2 px-4 focus:outline-none hover:bg-red-300 rounded text-base mt-4 md:mt-0 ml-10'>
+          Logout
+        </button>
       </div>
     </header>
   )
