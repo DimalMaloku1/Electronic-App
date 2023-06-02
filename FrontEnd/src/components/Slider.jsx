1861302edc1fe75e82492a6a282c728915c870e3
AndiZogaj22 <az51520@ubt-uni.net>
2023-06-02 23:34:39 +0200

    fixed slider

--- FrontEnd/src/components/Slider.jsx
+++ FrontEnd/src/components/Slider.jsx
@@ -1,22 +1,50 @@
-import ImageSlider from "../../src/components/ImageSlider";
+import React, { useState } from 'react';
+import img1 from '../assets/img1.png';
+import img2 from '../assets/img2.png';
+import img3 from '../assets/img3.png';
+import img4 from '../assets/img4.png';
+import img5 from '../assets/img5.png';
+
 const Slider = () => {
-  const slides = [
-    { url: "http://localhost:3000/img1.jpg", title: "foto1" },
-    { url: "http://localhost:3000/img2.jpg", title: "foto2" },
-    { url: "http://localhost:3000/img3.jpg", title: "foto3" },
-    { url: "http://localhost:3000/img4.jpg", title: "foto4" },
-    { url: "http://localhost:3000/img5.jpg", title: "foto5" },
-  ];
-  const containerStyles = {
-    width: "1800px",
-    height: "750px",
-    margin: "0 auto",
+  const images = [img1, img2, img3, img4, img5];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToPreviousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
   };
+
+  const goToNextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
-    <div>
-      <div style={containerStyles}>
-        <ImageSlider slides={slides} />
-      </div>
+    <div className="relative">
+            <h1 className="text-3xl font-bold text-center mb-4">Electronic E-Commerce</h1>
+
+      <img
+        src={images[currentImageIndex]}
+        alt="Slider Image"
+        className="object-contain h-3/5 w-screen transition-opacity duration-500"
+        style={{ maxHeight: '70vh' }}
+      />
+      <button
+        onClick={goToPreviousImage}
+        className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-blue-400 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-l transition-colors duration-300"
+        style={{ transitionProperty: 'background-color' }}
+      >
+        Previous
+      </button>
+      <button
+        onClick={goToNextImage}
+        className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-blue-400 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-r transition-colors duration-300"
+        style={{ transitionProperty: 'background-color' }}
+      >
+        Next
+      </button>
     </div>
   );
 };
