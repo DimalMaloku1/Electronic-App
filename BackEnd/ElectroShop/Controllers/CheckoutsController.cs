cde82757e3d52cf82b9269a93ad823ad648c8f81
DimalMaloku1 <dm51522@ubt-uni.net>
2023-06-14 23:03:49 +0200

    Created Cart/Checkout Function

---
+++ BackEnd/ElectroShop/Controllers/CheckoutsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+using Electroshop.Models;
+
+namespace Electroshop.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CheckoutController : ControllerBase
+    {
+        private readonly IMongoCollection<CheckoutData> _checkoutDataCollection;
+
+        public CheckoutController(IMongoClient client)
+        {
+            var database = client.GetDatabase("Electroshop");
+            _checkoutDataCollection = database.GetCollection<CheckoutData>("checkouts");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCheckouts()
+        {
+            try
+            {
+                var checkouts = await _checkoutDataCollection.Find(_ => true).ToListAsync();
+
+                return Ok(checkouts);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCheckout([FromBody] CheckoutData checkoutData)
+        {
+            try
+            {
+                checkoutData.Id = ObjectId.GenerateNewId().ToString(); // Generate default ObjectId
+
+                decimal totalPrice = 0;
+                foreach (var product in checkoutData.Products)
+                {
+                    totalPrice += product.Price * product.Quantity;
+                }
+                checkoutData.TotalPrice = totalPrice;
+
+                await _checkoutDataCollection.InsertOneAsync(checkoutData);
+
+                return Ok(checkoutData);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+    }
+}
