8abe3dade37de0ced2d6741c03233794098e357c
DimalMaloku1 <dm51522@ubt-uni.net>
2023-06-17 21:28:42 +0200

    Authorization Functionality with JWT

--- FrontEnd/src/modules/LoginRegister/Login.jsx
+++ FrontEnd/src/modules/LoginRegister/Login.jsx
@@ -3,96 +3,79 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const Login = () => {
-  const [username, usernameupdate] = useState('');
-  const [password, passwordupdate] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-  const usenavigate=useNavigate();
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
 
-  useEffect(()=>{
-sessionStorage.clear();
-  },[]);
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      const credentials = {
+        email: username,
+        password: password
+      };
 
-  const ProceedLogin = (e) => {
-      e.preventDefault();
-      if (validate()) {
-          ///implentation
-          // console.log('proceed');
-          fetch("http://localhost:8000/user/" + username).then((res) => {
-              return res.json();
-          }).then((resp) => {
-              //console.log(resp)
-              if (Object.keys(resp).length === 0) {
-                  toast.error('Please Enter valid username');
-              } else {
-                  if (resp.password === password) {
-                      toast.success('Success');
-                      sessionStorage.setItem('username',username);
-                      sessionStorage.setItem('userrole',resp.role);
-                      usenavigate('/')
-                  }else{
-                      toast.error('Please Enter valid credentials');
-                  }
-              }
-          }).catch((err) => {
-              toast.error('Login Failed due to :' + err.message);
-          });
-      }
-  }
+      fetch("https://localhost:7099/api/Account/login", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(credentials)
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error('Login failed');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          const { token } = data;
+          toast.success('Success');
+          localStorage.setItem('jwttoken', token); // Store token in local storage
+          localStorage.setItem('username', username); // Store username in local storage
+          navigate('/');
+          
+        })
+        .catch((err) => {
+          toast.error('Login Failed due to: ' + err.message);
+        });
+    }
+  };
 
-  const ProceedLoginusingAPI = (e) => {
-      e.preventDefault();
-      if (validate()) {
-          ///implentation
-          // console.log('proceed');
-          let inputobj={"username": username,
-          "password": password};
-          fetch("https://localhost:44308/User/Authenticate",{
-              method:'POST',
-              headers:{'content-type':'application/json'},
-              body:JSON.stringify(inputobj)
-          }).then((res) => {
-              return res.json();
-          }).then((resp) => {
-              console.log(resp)
-              if (Object.keys(resp).length === 0) {
-                  toast.error('Login failed, invalid credentials');
-              }else{
-                   toast.success('Success');
-                   sessionStorage.setItem('username',username);
-                   sessionStorage.setItem('jwttoken',resp.jwtToken);
-                 usenavigate('/')
-              }
-              // if (Object.keys(resp).length === 0) {
-              //     toast.error('Please Enter valid username');
-              // } else {
-              //     if (resp.password === password) {
-              //         toast.success('Success');
-              //         sessionStorage.setItem('username',username);
-              //         usenavigate('/')
-              //     }else{
-              //         toast.error('Please Enter valid credentials');
-              //     }
-              // }
-          }).catch((err) => {
-              toast.error('Login Failed due to :' + err.message);
-          });
-      }
-  }
   const validate = () => {
-      let result = true;
-      if (username === '' || username === null) {
-          result = false;
-          toast.warning('Please Enter Username');
-      }
-      if (password === '' || password === null) {
-          result = false;
-          toast.warning('Please Enter Password');
-      }
-      return result;
+    let result = true;
+    if (username === '' || username === null) {
+      result = false;
+      toast.warning('Please enter a username');
+    }
+    if (password === '' || password === null) {
+      result = false;
+      toast.warning('Please enter a password');
+    }
+    return result;
+  };
+ 
+  const token = localStorage.getItem('jwttoken'); // Retrieve the token from storage
+
+fetch('https://localhost:7099/api/Account', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}` // Set the Authorization header with the token
   }
+})
+  .then(response => {
+    // Handle the response
+  })
+  .catch(error => {
+    // Handle errors
+  });
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <form onSubmit={ProceedLogin} className="bg-white p-10 rounded-lg shadow-lg">
+      <form onSubmit={handleLogin} className="bg-white p-10 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold mb-5">Login</h2>
         <div className="mb-5">
           <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
@@ -102,7 +85,8 @@ sessionStorage.clear();
             type="email"
             id="email"
             className="border-2 border-gray-400 p-2 w-full rounded-md"
-            value={username} onChange={e => usernameupdate(e.target.value)}
+            value={username}
+            onChange={e => setUsername(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -112,7 +96,9 @@ sessionStorage.clear();
           <input
             id="password"
             className="border-2 border-gray-400 p-2 w-full rounded-md"
-            type="password" value={password} onChange={e => passwordupdate(e.target.value)}
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
         </div>
         <div className="flex justify-between items-center">
