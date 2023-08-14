c3efa61061ac09448e7d0c6262b51a6f7cb13894
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-14 18:12:44 +0200

    Customer's Model

---
+++ BackEnd/ElectroShop/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace ElectroShop.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public List<CustomerAddresses> CustomerAddresses { get; set; }
+    }
+}
