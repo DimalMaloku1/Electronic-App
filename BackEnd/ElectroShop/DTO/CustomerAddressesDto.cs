f52a867b5989638543651d66d9343a1d3468e0a6
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-14 18:11:02 +0200

    Inserted Data Transfer Object for CustomerAddresses

---
+++ BackEnd/ElectroShop/DTO/CustomerAddressesDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ElectroShop.DTO;
+
+    public class CustomerAddressesDto
+    {
+    public int Id { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public int CustomerId { get; set; }
+}
+
