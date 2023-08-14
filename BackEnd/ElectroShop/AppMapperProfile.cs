52a6597659dc396e8cd7c21852a4a0cf3c39a0af
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-14 18:09:18 +0200

    Created AppMapperProfile to Navigate

---
+++ BackEnd/ElectroShop/AppMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ElectroShop.DTO;
+using ElectroShop.Models;
+
+namespace ElectroShop;
+
+public class AppMapperProfile : Profile
+{
+    public AppMapperProfile()
+    {
+        CreateMap<CustomerDto, Customer>();
+        CreateMap<CustomerAddressesDto, CustomerAddresses>();
+    }
+}
+
