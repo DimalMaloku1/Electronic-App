652c45d3980770d7c1c344f3532e9a2aaf848149
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-14 18:11:37 +0200

    Created a DbContext For One-To-Many Relationship

---
+++ BackEnd/ElectroShop/Data/MyWorldDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ElectroShop.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ElectroShop.Data;
+public class MyWorldDbContext : DbContext
+{
+    public MyWorldDbContext(DbContextOptions<MyWorldDbContext> context) : base(context)
+    {
+    }
+    public DbSet<Customer> Customer { get; set; }
+    public DbSet<CustomerAddresses> CustomerAddresses { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CustomerAddresses>()
+        .HasOne(_ => _.Customer)
+        .WithMany(a => a.CustomerAddresses)
+        .HasForeignKey(p => p.CustomerId);
+    }
+}
