2eb6db3d8f90b95456b191da5884c745ad4348b0
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-14 18:11:14 +0200

    Data Transfer Object for Customer

---
+++ BackEnd/ElectroShop/DTO/CustomerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ElectroShop.DTO;
+public class CustomerDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public List<CustomerAddressesDto> CustomerAddresses { get; set; }
+}
