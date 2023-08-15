5321d797b63dc9fef5ee046e12c4c440094c61db
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-15 20:28:12 +0200

    Added Review Products and Rating

---
+++ BackEnd/ElectroShop/Models/Review.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using NSwag.Annotations;
+using System.Text.Json.Serialization;
+
+namespace ElectroShop.Models
+{
+    public class Review
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("productId")]
+        public string ProductId { get; set; }
+
+        [BsonElement("reviewText")]
+        public string ReviewText { get; set; }
+
+        [BsonElement("rating")]
+        public int Rating { get; set; }
+
+        [JsonIgnore]
+        [BsonIgnore]
+        public Product Product { get; set; }
+
+    }
+}
