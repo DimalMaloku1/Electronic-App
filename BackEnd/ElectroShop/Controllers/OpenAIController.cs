502fba255066db43904ad70ef198ad64f060da5e
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-15 20:27:29 +0200

    Created Function OpenAI bot

---
+++ BackEnd/ElectroShop/Controllers/OpenAIController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace ElectroShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAIController : ControllerBase
+    {
+        [HttpPost]
+        [Route("getresult")]
+        public IActionResult GetResult([FromBody] string prompt)
+        {
+            string apikey = "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";
+            string answer = string.Empty;
+            var openai = new OpenAIAPI(apikey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = prompt;
+            completion.Model = OpenAI_API.Models.Model.DavinciText;
+            completion.MaxTokens = 100;
+            var result = openai.Completions.CreateCompletionsAsync(completion);
+            if (result != null)
+            {
+                foreach (var item in result.Result.Completions)
+                {
+                    answer = item.Text;
+                }
+                return Ok(answer);
+            }
+            else
+            {
+                return BadRequest("Not Found");
+            }
+        }
+    }
+}
