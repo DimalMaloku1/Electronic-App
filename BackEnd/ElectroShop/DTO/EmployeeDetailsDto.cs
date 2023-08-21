2372ae37334c2c65579f2159fd06b02442ab736d
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-21 16:26:57 +0200

    EmployeeDetails properties for AppMapper

---
+++ BackEnd/ElectroShop/DTO/EmployeeDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿using ElectroShop.Models;
+
+namespace ElectroShop.DTO
+{
+    public class EmployeeDetailsDto
+    {
+        public int EmployeeDetailsID { get; set; }
+        public int EmployeeID { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
