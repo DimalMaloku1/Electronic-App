4ef407c60791666e557dace5c88e91498391ab3f
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-21 16:28:51 +0200

    Conversation Model Class

---
+++ BackEnd/ElectroShop/Models/Conversation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElectroShop.Models
+{
+    public class Conversation
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string UserId { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
