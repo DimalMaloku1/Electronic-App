e074684be8109e18348bd9991e782301fbfd66ca
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-21 16:27:11 +0200

    EmployeeDto properties for AutoMapper

---
+++ BackEnd/ElectroShop/DTO/EmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ElectroShop.DTO
+{
+    public class EmployeeDto
+    {
+    public int EmployeeID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public EmployeeDetailsDto EmployeeDetails { get; set; }
+    }
+}
