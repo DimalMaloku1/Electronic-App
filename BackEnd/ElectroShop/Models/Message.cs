bcd4eb0c4807ad0bc09780939a68c96416313869
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-21 16:29:26 +0200

    Message Model Class for OpenAI

---
+++ BackEnd/ElectroShop/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ElectroShop.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int ConversationId { get; set; }
+        public string UserId { get; set; }
+        public string UserMessage { get; set; }
+        public string BotReply { get; set; }
+        public Conversation Conversation { get; set; }
+    }
+}
