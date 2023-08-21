ab20766a585e83b1ba40abdc215ec18927040427
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-21 16:29:02 +0200

    Employee Model Class

---
+++ BackEnd/ElectroShop/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace ElectroShop.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public EmployeeDetails EmployeeDetails { get; set; }
+    }
+}
