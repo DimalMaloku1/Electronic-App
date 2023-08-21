d1bb45e2cbb05c03ddb2e21fbf54e6f3efa5f9fd
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-21 16:27:48 +0200

    Deliver a Message to OpenAI

---
+++ BackEnd/ElectroShop/DTO/MessageDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ElectroShop.DTO
+{
+    public class MessageDTO
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int ConversationId { get; set; }
+        public string UserId { get; set; }
+        public string UserMessage { get; set; }
+        public string BotReply { get; set; }
+    }
+}
