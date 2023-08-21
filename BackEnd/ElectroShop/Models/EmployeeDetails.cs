802647fcc44a6514c708a0551db8e9eef52a8232
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-21 16:29:13 +0200

    EmployeeDetails Model Class

---
+++ BackEnd/ElectroShop/Models/EmployeeDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectroShop.Models
+{
+    public class EmployeeDetails
+    {
+        [Key]
+        public int EmployeeDetailsID { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        // Foreign key property
+        public int EmployeeID { get; set; }
+        // Navigation property for Employee
+        public Employee Employee { get; set; }
+    }
+}
