82839b9239bb027102b3c8df5e2485bd204261ee
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-21 16:28:37 +0200

    Inserting a modelBuilder to Guide One-to-One entity relationships

---
+++ BackEnd/ElectroShop/Data/EmployeeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ElectroShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElectroShop.Data;
+
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> context) : base(context)
+        {
+        }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<EmployeeDetails> EmployeeDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.EmployeeDetails)
+                .WithOne(ed => ed.Employee)
+                .HasForeignKey<EmployeeDetails>(ed => ed.EmployeeID);
+        }
+    }
+
