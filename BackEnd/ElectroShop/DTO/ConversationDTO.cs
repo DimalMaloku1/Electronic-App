8767d001704a8f3ca95d471da49a6f0a70bc97c8
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-21 16:26:40 +0200

    Conversation that saves messages of the specific Users

---
+++ BackEnd/ElectroShop/DTO/ConversationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ElectroShop.DTO
+{
+    public class ConversationDTO
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string UserId { get; set; }
+        public List<MessageDTO> Messages { get; set; }
+    }
+}
