c5abd34e964c4ac16fc42c8c19f8c6951b2927d4
DimalMaloku1 <dm51522@ubt-uni.net>
2023-08-22 17:33:20 +0200

    Edited EmployeeDetails

--- FrontEnd/src/CrudLab/EmployeeDetailsListing.jsx
+++ FrontEnd/src/CrudLab/EmployeeDetailsListing.jsx
@@ -7,7 +7,7 @@ const EmployeeDetailsListing = () => {
 
     
     const LoadEdit = (id) => {
-      navigate(`/employeedetails/edit/${id}`); // Use backticks (`) to interpolate the ID
+      navigate("/employeedetails/edit/"+ id); // Use backticks (`) to interpolate the ID
     }
     const Removefunction = (id) => {
       if (window.confirm('Do you want to remove?')) {
@@ -45,7 +45,7 @@ const EmployeeDetailsListing = () => {
         fetch("https://localhost:7099/api/EmployeeDetails").then((res) => {
             return res.json();
         }).then((resp) => {
-            employeedetailsdatachange(resp);
+          employeedetailsdatachange(resp);
         }).catch((err) => {
             console.log(err.message);
         })
@@ -54,8 +54,9 @@ const EmployeeDetailsListing = () => {
     return (
         <div className="overflow-x-auto">
         <div className="mb-4 flex justify-between p-2">
-        <button className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded">
-          <Link to="/employeedetails/create">Add New</Link>
+        <button>
+          <Link to="/employeedetails/create"
+          className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded">Add New</Link>
         </button>
         <Link
           to="/adminproducts"
@@ -68,7 +69,7 @@ const EmployeeDetailsListing = () => {
         <thead>
           <tr>
             <th className="py-3 px-6 bg-gray-100 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
-              EmployeeDetials ID
+              EmployeeDetails ID
             </th>
             <th className="py-3 px-6 bg-gray-100 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
               Address
