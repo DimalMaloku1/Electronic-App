3ea7ad46f8e0ddc14dc3514ea3d3c0ca6ea8d796
AndiZogaj22 <andiolsizogaj@gmail.com>
2024-03-27 00:05:35 +0100

    CRA to vite migration

---
+++ FrontEnd/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import React from "@vitejs/plugin-react";
+
+export default defineConfig ({
+    plugins: [React()],
+    server: {
+        port: 3000,
+        open: true
+    }
+})
